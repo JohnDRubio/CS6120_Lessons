8e4c1677417b2ef89a974530197e3987c7961a0e
Arjun Shah <ans96@cornell.edu>
2023-08-30 20:29:33 -0400

    implemented methods to clean blocks and add labels

--- cfg.py
+++ cfg.py
@@ -3,7 +3,6 @@ import json
 file = open('myCode.json')
 data = json.load(file)
 
-
 def formBasicBlocks(insns):
     basicBlocks = []
     currBlock = []
@@ -12,14 +11,31 @@ def formBasicBlocks(insns):
             basicBlocks.append(currBlock)
             currBlock = []
             currBlock.append(i)
-        elif 'br' in i['op'] or 'jmp' in i['op']:
+        elif i['op'] == 'br' or i['op'] == 'jmp':
             currBlock.append(i)
             basicBlocks.append(currBlock)
             currBlock = []
         else:
             currBlock.append(i)
     basicBlocks.append(currBlock)
+    basicBlocks = removeEmptyBasicBlocks(basicBlocks)
+    basicBlocks = addLabels(basicBlocks)
     return basicBlocks
+
+def addLabels(basicBlocks):
+    num = 0
+    for block in basicBlocks:
+        if 'label' not in block[0]:
+            block.insert(0,{"label": "label_"+str(num)})
+            num = num + 1
+    return basicBlocks
+
+def removeEmptyBasicBlocks(basicBlocks):
+    newBasicBlocks = [x for x in basicBlocks if x != []]
+    return newBasicBlocks
+
+def createCFG(basicBlocks):
+    pass
 
 for i in data['functions']:
     blocks = formBasicBlocks(i['instrs'])
