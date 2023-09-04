c68e92ae8ae78664030a7b7b6d6e8699e79c2ba6
JohnDRubio <rubiojohn717@gmail.com>
2023-09-04 19:45:43 -0400

    saving this version before adding while loop

--- lesson03/tdce.py
+++ lesson03/tdce.py
@@ -6,13 +6,28 @@ import blocks
 ##TODO: Implement trivial dead code elimination (v1)
 ##TODO: Modify blocks in place rather than creating a new list (if time allows)
 def tdce(blocks):
+    # pass 1: Within a block, delete vars that are def'd but never used
     newBlocks = []
     for block in blocks:
         remove = []
+        used = []
+        defined = []
+        changed = True
         for insn in block:
-            if 'dest' in insn and insn['dest'] == 'a':
-                remove.append(insn)
-        newBlock = [x for x in block if x not in remove ]
+            if 'dest' in insn:
+                defined.append(insn['dest'])
+            if 'args' in insn:
+                for var in insn['args']:
+                    used.append(var)
+        print('DEST')
+        print(str(defined))
+        print('ARGS')
+        print(str(used))
+        # pass 2: Any insn that's been def'd but never used gets removed
+        for insn in block:
+            if 'dest' in insn and insn['dest'] not in used:
+                remove.append(insn)                
+            newBlock = [ x for x in block if x not in remove ]
         # print("remove")
         # print(str(remove))
         newBlocks.append(newBlock)
