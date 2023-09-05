807fdeaca1141ecc982baa49d57762270cec1cd2
JohnDRubio <rubiojohn717@gmail.com>
2023-09-04 21:58:15 -0400

    Implemented first two trivial dead code elimination tasks

--- lesson03/tdce.py
+++ lesson03/tdce.py
@@ -3,46 +3,94 @@ import json
 import sys
 import blocks
 
-##TODO: Implement trivial dead code elimination (v1)
+'''
+Trivial Dead Code Elimination: Optimization #1
+    If a variable, v, is def'd but never used (and the def has no side effects),
+    remove def(v).
+
+    Example:
+        a: int = const 4                    a: int = const 4
+        b: int = const 2                    b: int = const 2
+        c: int = const 1        --->        d: int = add a b
+        d: int = add a b                    print d
+        e: int = add c d
+        print d
+'''
+
 ##TODO: Modify blocks in place rather than creating a new list (if time allows)
-def tdce(blocks):
-    # pass 1: Within a block, delete vars that are def'd but never used
+def tdce_opt1(blocks):
     newBlocks = []
+    # pass 1: Within a block, delete vars that are def'd but never used
     for block in blocks:
-        remove = []
-        used = []
-        defined = []
         changed = True
-        for insn in block:
-            if 'dest' in insn:
-                defined.append(insn['dest'])
-            if 'args' in insn:
-                for var in insn['args']:
-                    used.append(var)
-        print('DEST')
-        print(str(defined))
-        print('ARGS')
-        print(str(used))
-        # pass 2: Any insn that's been def'd but never used gets removed
-        for insn in block:
-            if 'dest' in insn and insn['dest'] not in used:
-                remove.append(insn)                
-            newBlock = [ x for x in block if x not in remove ]
-        # print("remove")
-        # print(str(remove))
+        newBlock = block
+        while changed:
+            changed = False
+            remove = []
+            used = []
+            defined = []
+            for insn in newBlock:
+                if 'dest' in insn:
+                    defined.append(insn['dest'])
+                if 'args' in insn:
+                    for var in insn['args']:
+                        used.append(var)
+            # pass 2: Any insn that's been def'd but never used gets removed
+            for insn in newBlock:
+                if 'dest' in insn and insn['dest'] not in used:
+                    remove.append(insn)  
+                    changed = True              
+            newBlock = [ x for x in newBlock if x not in remove ]
         newBlocks.append(newBlock)
     return newBlocks
+
+'''
+Trivial Dead Code Elimination: Optimization #2
+    Within a basic block, if a variable, v, is def'd two or more times in a row
+    before being used, only keep the most recent def(v) before use(v).
+
+    Example:
+        a: int = const 4                    a: int = const 5
+        a: int = const 5        --->        print a
+        print a
+
+'''
+def tdce_opt2(blocks):
+    newBlocks = []
+    for block in blocks:
+        changed = True
+        newBlock = block
+        last_def = {}
+        while changed:
+            changed = False
+            remove = []
+            for insn in newBlock:
+                # check for uses
+                if 'args' in insn:
+                    for a in insn['args']:
+                        if a in last_def:
+                            del last_def[a]             
+                # check for defs
+                if 'dest' in insn:
+                    if insn['dest'] not in last_def:
+                        last_def[insn['dest']] = insn
+                    else:
+                        remove.append(last_def[insn['dest']])
+                        changed = True
+                        last_def[insn['dest']] = insn
+                        # newBlock = [ x for x in newBlock if x not in remove ]
+            newBlock = [ x for x in newBlock if x not in remove ]
+        newBlocks.append(newBlock)
+    return newBlocks
+
 
 def main():
     program = json.load(sys.stdin)
     for func in program['functions']:
         basicBlocks = blocks.formBasicBlocks(func)
-        # print(len(basicBlocks))
-        basicBlocks = tdce(basicBlocks)
-        # print(len(basicBlocks))
-        # blocks.printBasicBlocks(basicBlocks)
+        basicBlocks = tdce_opt1(basicBlocks)
+        basicBlocks = tdce_opt2(basicBlocks)
         func['instrs'] = list(itertools.chain(*basicBlocks))
-        # print(str(func))
     json.dump(program, sys.stdout, indent=2, sort_keys=True)
 
 if __name__ == "__main__":
