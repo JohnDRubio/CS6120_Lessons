a2121614a4c94f7dcd08f7d919318e1beebf66c6
JohnDRubio <rubiojohn717@gmail.com>
2023-09-05 15:22:34 -0400

    Starting preliminary LVN skeleton

---
+++ lesson03/Table.py
@@ -0,0 +1,20 @@
+class Table:
+    def __init__(self):
+        self.table = {}
+        self.var2num = {}
+        self.nrows = 0
+    
+    def __str__(self):
+        return f"VALUE -> (VAR, NUM): {str(self.table)}, VAR -> NUM: {str(self.var2num)}"
+
+test = Table()
+
+value = "ADD", 1, 2
+
+test.table[value] = "a", test.nrows
+
+test.var2num["a"] = test.nrows
+
+test.nrows += 1
+
+print(test)
---
+++ lesson03/lvn.py
@@ -0,0 +1,11 @@
+import tdce
+import itertools
+import json
+import sys
+import blocks
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
