31f8c92b50efa31c7fa44b9f3fa69c6665c1464b
JohnDRubio <rubiojohn717@gmail.com>
2023-09-19 12:50:43 -0400

    Constructing dominator trees correctly

--- lesson05/dominators.py
+++ lesson05/dominators.py
@@ -26,6 +26,36 @@ def getDominators(c, predecessors):
             changing = prevDom == dom
     return dom
 
+def doesStrictlyDominate(A, B, dom):
+    return A in dom[B] and A != B
+
+def doesImmediatelyDominate(A, B, dom):
+    if A not in dom[B]:
+        return False
+    
+    strictlyDominatesB = set()
+    for vertex in dom:
+        if doesStrictlyDominate(vertex, B, dom):
+            strictlyDominatesB.add(vertex)
+
+    for vertex in strictlyDominatesB:
+        if doesStrictlyDominate(A, vertex, dom):
+            return False
+
+    return True
+
+def getDominatorTree(dom):
+    domTree = {}
+    for vertex in dom:
+        for dominator in dom[vertex]:
+            if doesImmediatelyDominate(dominator,vertex,dom):
+                if dominator not in domTree:
+                    domTree[dominator] = set()
+                domTree[dominator].add(vertex)
+    return domTree
+
+
+
 def main():
     program = json.load(sys.stdin)
     # data = open('C:\\Users\\rubio\\Documents\\personal\\School\\CS6120\\lessons\\CS6120_Lessons\\lesson04\\reaching_def_test2.json')
@@ -34,7 +64,10 @@ def main():
         c = cfg.createCFG(func['instrs'])
         predecessors = cfg.buildPredecessorList(c)
         doms = getDominators(c, predecessors)
-        print(str(doms))
+        # print(str(doms))
+        # print(doesStrictlyDominate('label_0', 'label_0', doms))
+        # print(doesImmediatelyDominate('label_0', 'l4', doms))
+        print(str(getDominatorTree(doms)))
 
 if __name__ == "__main__":
     main()
