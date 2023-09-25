37aa922dcf29d888ddfae7115bfaa7ff10f1f873
Arjun Shah <ans96@cornell.edu>
2023-09-25 18:45:18 -0400

    code reuse

--- lesson06/ssa.py
+++ lesson06/ssa.py
@@ -1,30 +1,58 @@
-import cfg
 import sys
 import json
+sys.path.append("../library")
+sys.path.append("../lesson05")
+import cfg
 import graph
-import dominators 
+import dominators
+
+def getAllVars(insns):
+    vars = set()
+    for insn in insns:
+        if 'dest' in insn:
+            vars.add(insn['dest'])
+    return vars
+
+def getDefBlocks(insns):
+    defs = {}   # map from varName to set of blocks where var is def'd
+    basicBlocks = cfg.formBasicBlocks(insns)
+    for block in basicBlocks:
+        blockName = block[0]['label']
+        for insn in block:
+            if 'dest' in insn:
+                if insn['dest'] not in defs:
+                    defs[insn['dest']] = set()
+                    defs[insn['dest']].add(blockName)
+                else:
+                    defs[insn['dest']].add(blockName)
+    return defs
+
+def insertPhiNodes(vars,defs,df):
+    for v in vars:
+        for d in defs[v]:
+            for block in df[d]:
+                
 
 def main():
     program = json.load(sys.stdin)
     for func in program['functions']:
-        print(func['name']+' function')
+        print(func['name']+' function\n')
+
         c = cfg.createCFG(func['instrs'])
         predecessors = cfg.buildPredecessorList(c)
         doms = dominators.getDominators(c, predecessors)
 
-        vars = cfg.getAllVars(func['instrs'])                               # set of all variables in func
+        vars = getAllVars(func['instrs'])                               # set of all variables in func
         # print(str(vars))
-        defBlocks = cfg.getDefBlocks(func['instrs'])                        # map from varName -> set of blocks where varName is defined
+        defs = getDefBlocks(func['instrs'])                        # map from varName -> set of blocks where varName is defined
         # print(str(defBlocks))
-        successors = cfg.buildSuccessorList(c)                              # map from block, b, -> set of of b's successor blocks
-        # print(str(successors))
         domFrontier = dominators.getDominanceFrontier(doms, predecessors)   # map from block, b, -> set of blocks in b's dominance frontier
         # print(str(domFrontier))
         domTree = dominators.getDominatorTree(doms)                         # map from block, b, -> set blocks that b immediately dominates
         # print(str(domTree))
 
-        graph.createGraph(c,func['name']+"CFG")
-        graph.createGraph(dominators.getDominatorTree(doms),func['name']+"DomTree")
+        #graph.createGraph(c,func['name']+"CFG")
+        #graph.createGraph(dominators.getDominatorTree(doms),func['name']+"DomTree")
 
 if __name__ == "__main__":
     main()
