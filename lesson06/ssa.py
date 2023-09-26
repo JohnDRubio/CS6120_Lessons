b0c7683e8eeb153c551e9b328d014539925b397b
Arjun Shah <ans96@cornell.edu>
2023-09-25 21:29:22 -0400

    fix bugs in add phi node

--- lesson06/ssa.py
+++ lesson06/ssa.py
@@ -27,27 +27,40 @@ def getDefBlocks(insns):
                     defs[insn['dest']].add(blockName)
     return defs
 
+def getBlock(block_label,blocks):
+    for b in blocks:
+         if b[0]['label'] == block_label:
+             return b
+
 def addPhiNode(var,block,blocks,predsList):
     preds = predsList[block]
     phiNode = {"args": [var]*len(preds), "dest": var, "labels": preds, "op": "phi", "type": "int"}
-    for b in blocks:
-        if b[0]['label'] == block:
-            blocks.insert(1,phiNode)
+    b = getBlock(block,blocks)
+    b.insert(1,phiNode)
+
+def usesVar(block,var,blocks):
+    b = getBlock(block,blocks)
+    for insn in b:
+        if 'args' in insn:
+            if var in insn['args']:
+                return True
+    return False
 
 def insertPhiNodes(vars,defs,df,blocks,preds):
     phis = {} # l0: [a,b,c] , l1: [a] , ...
     for v in vars:
         for d in defs[v]:
-            for block in df[d]:
-                if block in defs[v]:
-                    if block not in phis:
-                        phis[block] = set()
-                        phis[block].add(v)
-                        addPhiNode(v,block,blocks,preds)
-                    else:
-                        if v not in phis[block]:
+            if d in df:
+                for block in df[d]:
+                    if usesVar(block,v,blocks):
+                        if block not in phis:
+                            phis[block] = set()
                             phis[block].add(v)
                             addPhiNode(v,block,blocks,preds)
+                        else:
+                            if v not in phis[block]:
+                                phis[block].add(v)
+                                addPhiNode(v,block,blocks,preds)
 
 def main():
     program = json.load(sys.stdin)
@@ -63,10 +76,24 @@ def main():
         defs = getDefBlocks(func['instrs'])                        # map from varName -> set of blocks where varName is defined
         domFrontier = dominators.getDominanceFrontier(doms, predecessors)   # map from block, b, -> set of blocks in b's dominance frontier
 
+        print('vars')
+        print(vars)
+        print('\n')
+
+        print('defs')
+        print(defs)
+        print('\n')
+
+        print('domFrontier')
+        print(domFrontier)
+        print('\n')
+
         insertPhiNodes(vars,defs,domFrontier,blocks,predecessors)
 
-        print(blocks)
-        print('\n')
+        print('blocks')
+        for block in blocks:
+            print(block)
+            print('\n')
 
         #graph.createGraph(c,func['name']+"CFG")
         #graph.createGraph(dominators.getDominatorTree(doms),func['name']+"DomTree")
