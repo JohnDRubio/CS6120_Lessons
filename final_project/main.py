7de4d727e65dc1f192657aa4b5055a31824736e0
JohnDRubio <rubiojohn717@gmail.com>
2023-10-31 16:22:34 -0400

    Notes from 10/31 meeting

--- final_project/main.py
+++ final_project/main.py
@@ -7,7 +7,16 @@ if __name__ == "__main__":
 '''
 Notes about classes / structure:
 
-Heirarchy for Bril Instructions
+Hierarchy for a Bril Program
+-List of Bril Functions
+
+Hierarchy for Bril Functions
+-Args
+-RetVal
+-Body (List of BrilInsns)
+
+Heierarchy for Bril Instructions
+ - Label
  - BrilInsn
     - ConstInsn
         - IntegerLiteral
@@ -36,5 +45,37 @@ Heirarchy for Bril Instructions
         - Ret
   - DummyInsn
       - Bril instructions that we are ignoring -> basically no-ops (print, memory, etc.)
+
+
+
+Hierarchy for RISC-V IR Functions
+-Label
+-Args
+-RetVal
+-Body (List of RISCV_IR_INSN)
+
+Hierarchy for RISC-V IR Instructions:
+- Label
+- RISCV_IR_INSN
+    - RegRegInsn
+        - Add
+        - Mul
+        - Sub
+        - Div
+        - And
+        - Or
+    - RegImmInsn
+        - Addi
+        - Subi
+        - Xori
+    - Conditional Branch
+        - Beq
+        - Blt
+        - Bgt
+        - Ble
+        - Bge
+    - Jump
+        - Jump and Link
+        - Jump and Link Register
 
 '''
