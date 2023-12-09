ee71a9d2f18b2ea06f760e7dab008d3dae7b77c2
Arjun Shah <ans96@cornell.edu>
2023-12-08 19:12:42 -0500

    finished writing insns before and after, left to do is update actual insn

--- final_project/TrivialRegAlloc/StackInsnAdder.py
+++ final_project/TrivialRegAlloc/StackInsnAdder.py
@@ -9,27 +9,28 @@ class StackInsnAdder():
     self.mapping = mapping
 
   def trivialRegisterAllocation(self):
+    # TODO: these functions below just add the insns before and after, still need
+    # to update the actual instruction to use the RISCV registers
+    
     pass
 
-
-  # TODO: these functions just add the insns before and after, still need
-  # to update the actual instruction to use the RISCV registers
-
   def addInsnsBefore(self,insn):
-    uses = insn.uses() 
     beforeInsns = []
+    uses = insn.uses() 
     currReg = 5
     for temp in uses:
       offset = self.mapping.getOffset(temp)
       beforeInsns.append(RVIRMemInsn('lw','x'+currReg,'fp', offset))
       currReg += 1
 
-    # TODO: what do we do after this?
-    return beforeInsns
+    # currReg here is the destination register to use
+    return currReg, beforeInsns
 
-  def addInsnsAfter(self,insn):
-    writes = insn.writes()
+  def addInsnsAfter(self,insn,dest):
+    # write value in the dest register to the location of the write temp
     afterInsns = []
-
+    write = insn.writes()[0] # should only be one value
+    offset = self.mapping.getOffset(write)
+    afterInsns.append(RVIRMemInsn('sw','x'+dest,'fp', offset))
 
     return afterInsns
