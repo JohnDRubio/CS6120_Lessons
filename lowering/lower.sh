e9f1451d781806088be070de4dfb57f92df63d1f
JohnDRubio <rubiojohn717@gmail.com>
2023-12-12 13:06:45 -0500

    Clean up

--- lowering/lower.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Change to the directory containing the Bril files
-cd ../test/benchmarks/core/
-
-# Run the command on all files
-for file in *.bril; do
-    bril2json < "$file" | python main.py -o "${file%.bril}.asm"
-done
